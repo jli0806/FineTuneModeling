f6ddf9571b778f4baee1a85339c8cc77973c708f
jli0806 <joyceli.li@gmail.com>
2023-05-26 15:32:33 -0700

    version 1.0 fine tune AI model

---
+++ index.js
@@ -0,0 +1,61 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+//temp solution: to remove the User_Agent error due to unsafe API key exposure at the moment.
+//delete configuration.baseOptions.headers['User-Agent']
+//end of temp solution
+
+const openai = new OpenAIApi(configuration)
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+ 
+let conversationStr = ''
+ 
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input')   
+    conversationStr += ` ${userInput.value} ->`
+
+    fetchReply()
+    
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+    userInput.value = ''
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+}) 
+
+async function fetchReply(){
+    const response = await openai.createCompletion({
+        model: 'davinci:ft-averanda-partners-2023-05-26-18-57-22',
+        prompt: conversationStr,
+        presence_penalty: 0,
+        frequency_penalty: 0.3,
+        max_tokens: 100,
+        temperature: 0,
+        stop: ['->',' END']
+    }) 
+
+    conversationStr += ` ${response.data.choices[0].text} END` 
+    renderTypewriterText(response.data.choices[0].text)
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i-1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
